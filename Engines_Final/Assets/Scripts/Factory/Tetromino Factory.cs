60b78858f185204a3209ca07faef04152165f074
Jayden Deller-Daoust <jayden.dellerdaoust@gmail.com>
2024-12-04 12:42:59 -0500

    GameManager and Tilemap Factory

---
+++ Engines_Final/Assets/Scripts/Factory/Tetromino Factory.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class TetrominoFactory : MonoBehaviour
+{
+
+    // Cubes representing the shapes
+    [SerializeField] GameObject[] L_Block;
+    [SerializeField] GameObject[] T_Block;
+    [SerializeField] GameObject[] J_Block;
+    [SerializeField] GameObject[] S_Block;
+    [SerializeField] GameObject[] Z_Block;
+    [SerializeField] GameObject[] O_Block;
+    [SerializeField] GameObject[] I_Block;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
